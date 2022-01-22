b8f106c31ae22e37ccf918f119ec9bcf789e7905
Piyush Keshri <piyushkeshri383@gmail.com>
2022-01-22 13:13:55 +0530

    commit

--- Object Oriented Programming/script.js
+++ Object Oriented Programming/script.js
@@ -88,6 +88,7 @@ console.log(lisa,jack,bob);
 console.log(jonas instanceof Person);
 
 //------------------PROTOTYPES.
+
 //The JavaScript prototype property allows you to add new properties to object constructors.
 
 console.log(Person.prototype);
@@ -117,4 +118,94 @@ Array.prototype.unique = function(){
   return [...new Set(this)];
 
 }
-console.log(arr.unique())
+console.log(arr.unique());
+
+//-----------------------Coding Challenge 1 -------------------------------------//
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+GOOD LUCK 😀
+*/
+
+const Car = function(make,speed){
+
+  this.make = make;
+  this.speed = speed;
+}
+
+Car.prototype.accelerate = function(){
+
+  this.speed +=10;
+  console.log(this.speed);
+}
+
+Car.prototype.brake = function(){
+
+  this.speed -=5;
+  console.log(this.speed);
+}
+
+const bmw = new Car('BMW',120);
+const mercedes = new Car('Mercedes',95);
+
+bmw.accelerate();
+mercedes.brake();
+
+//--------------------------------CLASSES------------------------------------//
+
+// Classes are not hoisted.
+// Classes are also first class citizens.(We can pass them into functions and also return them from functions.)
+// Classes are executed into strict mode.
+
+
+//Class expression
+
+// const PersonCl = class{} ;
+
+//Class declaration
+
+class PersonCl {
+
+  constructor(firstName,birthYear){
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge(){
+    console.log(2037 - this.birthYear);
+  }
+
+}
+
+const jessica = new PersonCl('Jessica',1996);
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+
+// ------------------SETTERS AND GETTERS----------------------- //
+const account = {
+
+  owner: 'Jonas',
+  movements: [200,530,120,300],
+
+  get latest(){
+
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov){
+    this.movements.push(mov);
+  }
+};
+
+console.log(account.latest);
+account.latest = 50;
+
+console.log(account.latest);
+
+
