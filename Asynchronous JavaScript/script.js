4ae5e43f4b5920aca95b5bced405706140d768f3
Piyush Keshri <piyushkeshri383@gmail.com>
2022-01-29 21:40:54 +0530

    commit

--- Asynchronous JavaScript/script.js
+++ Asynchronous JavaScript/script.js
@@ -20,8 +20,8 @@
 
 */
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+// const btn = document.querySelector('.btn-country');
+// const countriesContainer = document.querySelector('.countries');
 
 /////////////////////////////////////////////////////////////////////////////
 /*
@@ -157,6 +157,7 @@ const getCountryData = function(country){
   })
 }
 */
+/* 
 const getCountryData = function(country){
 
   //Country 1
@@ -181,29 +182,48 @@ const getCountryData = function(country){
 btn.addEventListener('click',function(){
   getCountryData('germany');
 });
-
-/* 
-In this challenge you will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. 
-For that, you will use a second API to geocode coordinates.
-Here are your tasks:
-PART 1
-1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS coordinates, examples are below).
-2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name.
-   Use this API to do reverse geocoding: https://geocode.xyz/api.
-   The AJAX call will be done to a URL with this format: https://geocode.xyz/52.508,13.381?geoit=json.
-   Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, that is cheating 😉
-3. Once you have the data, take a look at it in the console to see all the attributes that you recieved about the provided location.
-   Then, using this data, log a messsage like this to the console: 'You are in Berlin, Germany'
-4. Chain a .catch method to the end of the promise chain and log errors to the console
-5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403.
-   This is an error with the request. Remember, fetch() does NOT reject the promise in this case.
-   So create an error to reject the promise yourself, with a meaningful error message.
-   
-PART 2
-6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API that we have been using.
-7. Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, no need to type the same code)
-TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
-TEST COORDINATES 2: 19.037, 72.873
-TEST COORDINATES 2: -33.933, 18.474
-GOOD LUCK 😀
 */
+
+/*
+// Event loop in practice
+
+console.log('Test Start');
+setTimeout(() => console.log('0 sec timer'),0);
+Promise.resolve('Resolved Promise 1').then(res => console.log(res));
+console.log('Test End');
+
+Promise.resolve('Resolved Promise 2').then(res => {
+  for(let i=0;i<1000;i++){
+    console.log(res);
+  }
+});
+console.log('test end');
+*/
+
+// Building Simple Promises
+
+const lotteryPromise = new Promise(function(resolve,reject){
+console.log('Lottery Draw is happening 🪄');
+setTimeout(function(){
+if(Math.random() >= 0.5){
+  resolve('You Win 🤑');
+}
+else{
+  reject('You Lose 👎');
+}
+},2000);
+});
+lotteryPromise.then(res => console.log(res)).catch(err => console.log(err));
+
+//Promisifying setTimeout
+const wait = function(seconds){
+  return new Promise(function(resolve){
+
+    setTimeout(resolve, seconds*1000);
+
+  });
+}
+wait(3).then(() => {
+  console.log('I waited for 3 sec.')
+  return wait(1);
+});
