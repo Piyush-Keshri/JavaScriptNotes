afcde9d5d114fa8285329871d191d1438bd1b3f8
Piyush Keshri <piyushkeshri383@gmail.com>
2022-01-30 12:46:21 +0530

    commit

--- Asynchronous JavaScript/script.js
+++ Asynchronous JavaScript/script.js
@@ -199,7 +199,7 @@ Promise.resolve('Resolved Promise 2').then(res => {
 });
 console.log('test end');
 */
-
+/*
 // Building Simple Promises
 
 const lotteryPromise = new Promise(function(resolve,reject){
@@ -227,3 +227,50 @@ wait(3).then(() => {
   console.log('I waited for 3 sec.')
   return wait(1);
 });
+*/
+
+// Promisifying geolocation API
+/*
+const getPosition = function(){
+
+  return new Promise(function(resolve,reject){
+
+    // navigator.geolocation.getCurrentPosition(
+    //   position => resolve(position),
+    //   err => reject(err)
+    //   );
+
+    navigator.geolocation.getCurrentPosition(resolve,reject);
+
+  });
+}
+getPosition().then(pos => console.log(pos));
+
+*/ 
+/*
+PART 1
+1. Create a function 'createImage' which receives 'imgPath' as an input.
+This function returns a promise which creates a new image (use
+document.createElement('img')) and sets the .src attribute to the
+provided image path
+2. When the image is done loading, append it to the DOM element with the
+'images' class, and resolve the promise. The fulfilled value should be the
+image element itself. In case there is an error loading the image (listen for
+the'error' event), reject the promise
+3. If this part is too tricky for you, just watch the first part of the solution
+
+PART 2
+4. Consume the promise using .then and also add an error handler
+5. After the image has loaded, pause execution for 2 seconds using the 'wait'
+function we created earlier
+6. After the 2 seconds have passed, hide the current image (set display CSS
+property to 'none'), and load a second image (Hint: Use the image element
+returned by the 'createImage' promise to hide the current image. You will
+need a global variable for that 😉)
+7. After the second image has loaded, pause execution for 2 seconds again
+8. After the 2 seconds have passed, hide the current image
+Test data: Images in the img folder. Test the error handler by passing a wrong
+image path. Set the network speed to “Fast 3G” in the dev tools Network tab,
+otherwise images load too fast
+*/
+
